7aabda8fb76f53c0191a29bb5e264aed146398b5
Teymur Rzayev <teymur.rzayev.rt@gmail.com>
2025-03-14 21:30:46 +0400

    ds

--- lesson-3/task1.py
+++ lesson-3/task1.py
@@ -2,8 +2,10 @@ def flatten_arr(arr):
     flattened_arr=[]
     for arr1 in arr:
         if isinstance(arr1,list):
+            flattened_arr.append(arr1)
             for arr2 in arr:
                 if isinstance(arr2,list):
+                    flattened_arr.append(arr2)
                     for arr3 in arr:
                         if isinstance(arr3,list):
                             flattened_arr.append(arr3)
@@ -17,3 +19,4 @@ def flatten_arr(arr):
             arr1=list(arr1)
             flattened_arr.append(arr1)
     return flattened_arr
+print(flatten_arr)
